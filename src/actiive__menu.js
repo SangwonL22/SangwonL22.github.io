e2af57665ec9b71b5490b43f78fe73b404c421cd
SANGWON LEE <yonga3749@naver.com>
2023-07-26 08:33:38 +0900

    JS-observer

---
+++ src/actiive__menu.js
@@ -0,0 +1,53 @@
+'use strict';
+const sectionIds = [
+  '#home',
+  '#about',
+  '#skills',
+  '#work',
+  '#hobbies',
+  '#contact',
+];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[href="${id}"]`)
+);
+
+const visibleSections = sectionIds.map(() => false);
+let activeNavItem = navItems[0];
+const options = {
+  rootMargin: '-20% 0px 0px 0px',
+  threshold: [0, 0.98],
+};
+const observer = new IntersectionObserver(observerCallback, options);
+sections.forEach((section) => observer.observe(section));
+
+function observerCallback(entries) {
+  let seslectLastOne;
+  entries.forEach((entry) => {
+    const index = sectionIds.indexOf(`#${entry.target.id}`);
+    visibleSections[index] = entry.isIntersecting;
+    seslectLastOne =
+      index === sectionIds.length - 1 &&
+      entry.isIntersecting &&
+      entry.intersectionRatio >= 0.95;
+  });
+
+  const navIndex = seslectLastOne
+    ? sectionIds.length - 1
+    : findFirstIntersecting(visibleSections);
+
+  selectNavItem(navIndex);
+}
+
+function findFirstIntersecting(Intersections) {
+  const index = Intersections.indexOf(true);
+  return index >= 0 ? index : 0;
+}
+
+function selectNavItem(index) {
+  const navItem = navItems[index];
+  if (!navItem) return;
+  activeNavItem.classList.remove('active');
+  activeNavItem = navItem;
+  activeNavItem.classList.add('active');
+}
