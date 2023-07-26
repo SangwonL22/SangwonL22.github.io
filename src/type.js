572fa3c51c1691495319edcf4a4f2db1f8a97e2d
SANGWON LEE <yonga3749@naver.com>
2023-07-26 20:52:09 +0900

    CSS - edit TypeIt

--- src/type.js
+++ src/type.js
@@ -2,20 +2,25 @@
 
 new TypeIt('.home__title--strong', {
   loop: false,
-  speed: 100,
+  speed: 110,
 }) // 프론트엔드 개발자
   .move(-9)
   .type('코딩을 즐기는 ')
+  .pause(2000)
   .delete()
   .move(6)
   .delete()
   .type('백앤드도 하고 싶은 ')
+  .pause(1500)
   .delete()
   .type('풀스택이 목표인 ')
+  .pause(1500)
   .delete()
   .type('매일 고단히 공부하는 ')
+  .pause(1500)
   .move(null, { to: 'END' })
   .delete()
   .type('꿈을 설계하는 주니어 개발자 ')
+  .pause(3000)
   .delete()
   .go();
